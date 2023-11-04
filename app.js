8ba4d180b4703a022a8626353f9249a20f9dd7e7
sksunil0608 <sksunil0608@gmail.com>
2023-11-04 11:43:10 +0530

    Get Request header

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    console.log("Sunil Kumar Mahto")
+    console.log(req.url)
+    console.log(req.method)
+    console.log(req.headers)
+    // process.exit();
 })
 
 server.listen(4000);
