354bb4f31ce524fb819b5606074b300784a71cc9
sksunil0608 <sksunil0608@gmail.com>
2023-11-04 16:04:10 +0530

    Store data by http post method in a local file

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 const http = require("http");
+const fs = require("fs");
 
-// const server = http.createServer((req,res)=>{
-//     console.log(req.url)
-//     console.log(req.method)
-//     console.log(req.headers)
-//     res.setHeader('Conteny-Type','text/html');
-//     res.write('<html>');
-//     res.write('<head><title>First Node App</title></head>');
-//     res.write('<body><h1>This is My first Node App</h1></body>');
-//     res.write('</html>');
-// })
 const server = http.createServer((req, res) => {
-  if (req.url === "/home") {
-    res.setHeader("Conteny-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Home</title></head>");
-    res.write("<body><h1>Welcome Home</h1></body>");
-    res.write("</html>");
-  } 
-  else if (req.url === "/about") {
+    const url = req.url;
+    const method = req.method;
+    if (url === "/") {
         res.setHeader("Conteny-Type", "text/html");
         res.write("<html>");
-        res.write("<head><title>About</title></head>");
-        res.write("<body><h1>Welcome to About Us page</h1></body>");
+        res.write("<head><title>Register</title></head>");
+        res.write(`
+        <body>
+            <h1>Register</h1>
+            <form action="/message" method="post">
+                <input type ="text" name="message">
+                <button type="submit">Submit</button>
+            </form>
+        </body>`);
         res.write("</html>");
-  } else if (req.url === "/node") {
-    res.setHeader("Conteny-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Node App</title></head>");
-    res.write("<body><h1>Welcome to my NOde JS Project</h1></body>");
-    res.write("</html>");
-  }
+        return res.end();
+    } 
+    if(url==="/message" && method ==="POST"){
+        const body = [];
+        req.on('data',(chunk)=>{
+            // console.log(chunk);
+            body.push(chunk);
+        });
+
+        return req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            // console.log(parsedBody);
+            fs.writeFile("message.txt", message,(error=>error));
+            res.statusCode = 302;
+            res.setHeader('Location','/');
+            return res.end();
+        })
+
+    }
 });
 
 server.listen(4000);
