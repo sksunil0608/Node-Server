871337f1cb95335901c6ac2c30629c7aa0196d5a
sksunil0608 <sksunil0608@gmail.com>
2023-11-06 13:21:59 +0530

    added bodyParser and used get and post along with app

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const http = require("http");
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-app.use((req,res,next)=>{
-console.log("I am the first middleware!")
-next()
-})
-app.use((req, res, next) => {
-  console.log("I am the Second middleware!");
-  res.send({"name":"Sunil"});
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product',(req, res, next) => {
+    res.send(`<html>
+    <head><titile>Add Produt</title></head>
+    <body>
+    <form action="/product" method="POST">
+    <input type="text" name="title">
+    <button type="submit" >Add Product</button>
+    </form>
+    </html>`);
+});
+
+app.get('/product',(req, res, next) => {
+    console.log(req.body)
+    console.log(JSON.stringify(req.body))
+    res.redirect('/')
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<html><h1>This is Home Page</h1></html>")
 });
 const server = http.createServer(app);
     
