106aa213589c220b8f21fcc47b5f5fa87a3d5e07
sksunil0608 <sksunil0608@gmail.com>
2023-11-06 13:31:46 +0530

    Added second input box and fetching data of that

--- app.js
+++ app.js
@@ -12,14 +12,20 @@ app.use('/add-product',(req, res, next) => {
     <body>
     <form action="/product" method="POST">
     <input type="text" name="title">
+    <input type="text" name="author">
     <button type="submit" >Add Product</button>
     </form>
     </html>`);
 });
 
-app.get('/product',(req, res, next) => {
-    console.log(req.body)
-    console.log(JSON.stringify(req.body))
+app.post('/product',(req, res, next) => {
+    const form_data = req.body
+    // console.log(req.body)
+    // console.log(JSON.stringify(req.body))
+
+    console.log({title:form_data.title}, {Author:form_data.author})
+
+    
     res.redirect('/')
 });
 
