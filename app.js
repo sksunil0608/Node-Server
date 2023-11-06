2efa0d161e3d4364b48fcf967af052f3a82be54a
sksunil0608 <sksunil0608@gmail.com>
2023-11-06 11:33:22 +0530

    Installed Express JS

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const http = require("http");
-const routes = require('./routes')
-const server = http.createServer(routes.handler);
+const express = require('express');
+const app = express();
+app.use((req,res,next)=>{
+console.log("I am the first middleware!")
+next()
+})
+app.use((req, res, next) => {
+  console.log("I am the Second middleware!");
+  res.send('<h1>This is second MiddleWare</h1>')
+});
+const server = http.createServer(app);
     
 server.listen(4000);
