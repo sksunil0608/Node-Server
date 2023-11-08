3fdeaed8dec6b4a9be5481d423fd90a2683fa59d
sksunil0608 <sksunil0608@gmail.com>
2023-11-08 16:54:01 +0530

    Added Cart Module

--- routes/shop.js
+++ routes/shop.js
@@ -13,6 +13,7 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:prodId',shopController.getProduct)
 
 router.get('/cart', shopController.getCart);
+router.post('/cart',shopController.postCart);
 
 router.get('/orders', shopController.getOrders);
 
