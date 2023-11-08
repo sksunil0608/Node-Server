732157ab89a38d53d21a67f775c899d53f5564f4
sksunil0608 <sksunil0608@gmail.com>
2023-11-09 05:29:03 +0530

    Sequelize setup done server conntect to sql and workbech and sequelize

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./util/database')
+const sequelize = require('./util/database')
 
 const errorController = require('./controllers/error');
 
@@ -21,5 +21,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+sequelize.sync().then((result)=>{
+    // console.log(result)
+    app.listen(3000);
+}).catch(err=>{console.log(err)})
 
-app.listen(3000);
